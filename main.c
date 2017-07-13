ade7760d7d66ff6edade2b729c102cfcb6e355bd
Toni Spets <toni.spets@iki.fi>
2017-07-13 14:59:41 +0300

    Allow reuse of Lua state if -r <n> given

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <lauxlib.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #define NO_FCGI_DEFINES
 #include <fcgi_stdio.h>
@@ -21,7 +23,27 @@ int main(int argc, char **argv)
 {
     lua_State *L = NULL;
     int debug = 0;
+    int reuse = 0;
     const char *bootstrap_path = NULL;
+
+    int c;
+
+    /* parse options */
+    while ((c = getopt(argc, argv, "dr:")) >= 0) {
+        switch (c) {
+            case 'd':
+                debug = 1;
+                break;
+
+            case 'r':
+                reuse = strtol(optarg, NULL, 10);
+                break;
+
+            case '?':
+            default:
+                break;
+        }
+    }
 
 #ifdef WITH_CACHE
     time_t now, last_gc = 0;
@@ -30,34 +52,36 @@ int main(int argc, char **argv)
     cache_init();
 #endif
 
-    if (argc > 0) {
-        bootstrap_path = argv[1];
+    if (optind < argc) {
+        bootstrap_path = argv[optind];
     }
 
     while (1) {
-        L = luaL_newstate();
-        luaL_openlibs(L);
+        if (!L) {
+            L = luaL_newstate();
+            luaL_openlibs(L);
 
 #ifdef WITH_CACHE
-        /* run gc every minute if we happen to get a request */
-        now = time(NULL);
-        if (last_gc + 60 < now || debug) {
-            cache_gc(-1);
-            last_gc = now;
-        }
+            /* run gc every minute if we happen to get a request */
+            now = time(NULL);
+            if (last_gc + 60 < now || debug) {
+                cache_gc(-1);
+                last_gc = now;
+            }
 
-        /* install bytecode cache */
-        cache_install(L);
+            /* install bytecode cache */
+            cache_install(L);
 #endif
 
-        /* install I/O redirection */
-        redir_install(L);
+            /* install I/O redirection */
+            redir_install(L);
 
-        /* preload bootstrap script */
-        if (bootstrap_path) {
-            if (cache_loadfile(L, bootstrap_path, "") != LUA_OK) {
-                fprintf(stderr, "Loading bootstrap failed: %s\n", lua_tostring(L, 1));
-                break;
+            /* preload bootstrap script */
+            if (bootstrap_path) {
+                if (cache_loadfile(L, bootstrap_path, "") != LUA_OK) {
+                    fprintf(stderr, "Loading bootstrap failed: %s\n", lua_tostring(L, 1));
+                    break;
+                }
             }
         }
 
@@ -90,8 +114,12 @@ next:
         /* flush FCGI request */
         FCGI_Finish();
 
-        /* free state in-between requests */
-        lua_close(L);
+        // XXX: should be number of requests to process until throwing away the state
+        if (!reuse) {
+            /* free state in-between requests */
+            lua_close(L);
+            L = NULL;
+        }
     }
 
     lua_close(L);
